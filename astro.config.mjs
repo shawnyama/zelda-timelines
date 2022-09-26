07e1e999ab9cf6f874192a7962c0c6399aeb9f3b
Shawn Yama <yama.shawn@outlook.com>
2022-09-25 20:09:50 -0400

    basis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  integrations: [preact()]
+});
