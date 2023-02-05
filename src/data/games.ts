49f1b291d9f11cc49a6cbd7b09bf459cf860b683
Shawn Yama <yama.shawn@outlook.com>
2023-02-05 17:10:49 -0500

    data structure for games

--- src/data/games.ts
+++ src/data/games.ts
@@ -1,58 +1,442 @@
 export const nodes = [
-    { name: "Skyward Sword", imageName: "skyward-sword" },
-    { name: "Ocarina of Time", imageName: "ocarina-of-time" },
-    { name: "Majora's Mask", imageName: "majoras-mask" },
-    { name: "Hyrule Warriors", imageName: "hyrule-warriors" },
-    { name: "Twilight Princess", imageName: "twilight-princess" },
-    { name: "Link's Crossbow Training", imageName: "links-crossbow-training" },
-    { name: "A Link to the Past", imageName: "a-link-to-the-past" },
-    { name: "The Wind Waker", imageName: "the-wind-waker" },
-    { name: "Navi Trackers", imageName: "mdi:triforce" },
-    { name: "Phantom Hourglass", imageName: "phantom-hourglass" },
-    { name: "Spirit Tracks", imageName: "spirit-tracks" },
-    { name: "The Minish Cap", imageName: "the-minish-cap" },
-    { name: "Four Swords", imageName: "four-swords-ds" },
-    { name: "Four Swords Adventures", imageName: "four-swords-adventures" },
-    { name: "Ancient Stone Tablets", imageName: "mdi:triforce" },
-    { name: "A Link Between Worlds", imageName: "a-link-between-worlds" },
-    { name: "Cadence of Hyrule", imageName: "cadence-of-hyrule" },
-    { name: "Triforce Heroes", imageName: "triforce-heroes" },
-    { name: "Oracle of Seasons", imageName: "oracle-of-seasons" },
-    { name: "Oracle of Ages", imageName: "oracle-of-ages" },
-    { name: "Link's Awakening", imageName: "links-awakening" },
-    { name: "Zelda I", imageName: "the-legend-of-zelda" },
-    { name: "Zelda II: The Adventure of Link", imageName: "the-adventure-of-link" },
-    { name: "CDi Games & Cartoon", imageName: "mdi:triforce" },
-    { name: "Breath of the Wild", imageName: "breath-of-the-wild" },
-    { name: "Tears of the Kingdom", imageName: "tears-of-the-kingdom" },
-    { name: "Hyrule Warriors: Age of Calamity", imageName: "age-of-calamity" }
+    {
+        name: "Skyward Sword",
+        imageName: "skyward-sword",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Ocarina of Time",
+        imageName: "ocarina-of-time",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 2,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Majora's Mask",
+        imageName: "majoras-mask",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 3,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Hyrule Warriors",
+        imageName: "hyrule-warriors",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: null,
+            placement: null,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 4,
+        },
+        loruleanTimeline: {
+            branch: null,
+            placement: null,
+        }
+    },
+    {
+        name: "Twilight Princess",
+        isSpinOff: true,
+        imageName: "twilight-princess",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 5,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Link's Crossbow Training",
+        imageName: "links-crossbow-training",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 6
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "A Link to the Past",
+        imageName: "a-link-to-the-past",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "prime",
+            placement: 7,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "The Wind Waker",
+        imageName: "the-wind-waker",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "diverged",
+            placement: 1,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Navi Trackers",
+        imageName: "mdi:triforce",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "diverged",
+            placement: 2,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Phantom Hourglass",
+        imageName: "phantom-hourglass",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+             branch: "diverged",
+            placement: 3,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Spirit Tracks",
+        imageName: "spirit-tracks",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+           branch: "diverged",
+            placement: 4,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "The Minish Cap",
+        imageName: "the-minish-cap",
+        officialTimeline: {
+            branch: "prime",
+            placement: 5,
+        },
+        triforceTimeline: {
+           branch: "diverged",
+            placement: 2,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Four Swords",
+        imageName: "four-swords-ds",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+           branch: "diverged",
+            placement: 6,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Four Swords Adventures",
+        imageName: "four-swords-adventures",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "diverged",
+            placement: 7,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Ancient Stone Tablets",
+        imageName: "mdi:triforce",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 1,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "A Link Between Worlds",
+        imageName: "a-link-between-worlds",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 2,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Cadence of Hyrule",
+        imageName: "cadence-of-hyrule",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 3,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Triforce Heroes",
+        imageName: "triforce-heroes",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 4,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Oracle of Seasons",
+        imageName: "oracle-of-seasons",
+        officialTimeline: {
+            branch: "prime",
+            placement: 5,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 1,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Oracle of Ages",
+        imageName: "oracle-of-ages",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 6,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Link's Awakening",
+        imageName: "links-awakening",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 7,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Zelda I",
+        imageName: "the-legend-of-zelda",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 8,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Zelda II: The Adventure of Link",
+        imageName: "the-adventure-of-link",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 9,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "CDi Games & Cartoon",
+        imageName: "mdi:triforce",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 10,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Breath of the Wild",
+        imageName: "breath-of-the-wild",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 11,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Hyrule Warriors: Age of Calamity",
+        imageName: "age-of-calamity",
+        isSpinOff: true,
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "diverged2",
+            placement: 1,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    },
+    {
+        name: "Tears of the Kingdom",
+        imageName: "tears-of-the-kingdom",
+        officialTimeline: {
+            branch: "prime",
+            placement: 1,
+        },
+        triforceTimeline: {
+            branch: "converged",
+            placement: 12,
+        },
+        loruleanTimeline: {
+            branch: "prime",
+            placement: 1,
+        }
+    }
 ];
-//      "links": [
-//         {"source": "Skyward Sword", "target": "Ocarina of Time", "value": 1},
-//         {"source": "Ocarina of Time", "target": "Majora's Mask", "value": 1},
-//         {"source": "Majora's Mask", "target": "Hyrule Warriors", "value": 1},
-//         {"source": "Hyrule Warriors", "target": "Twilight Princess", "value": 1},
-//         {"source": "Twilight Princess", "target": "Link's Crossbow Training", "value": 1},
-//         {"source": "Link's Crossbow Training", "target": "A Link to the Past", "value": 1},
-//         {"source": "Ocarina of Time", "target": "The Wind Waker", "value": 1},
-//         {"source": "The Wind Waker", "target": "Navi Trackers", "value": 1},
-//         {"source": "Navi Trackers", "target": "Phantom Hourglass", "value": 1},
-//         {"source": "Phantom Hourglass", "target": "Spirit Tracks", "value": 1},
-//         {"source": "Spirit Tracks", "target": "The Minish Cap", "value": 1},
-//         {"source": "The Minish Cap", "target": "Four Swords", "value": 1},
-//         {"source": "Four Swords", "target": "Four Swords Adventures", "value": 1},
-//         {"source": "Four Swords Adventures", "target": "A Link to the Past", "value": 1},
-//         {"source": "A Link to the Past", "target": "Ancient Stone Tablets", "value": 1},
-//         {"source": "Ancient Stone Tablets", "target": "A Link Between Worlds", "value": 1},
-//         {"source": "A Link Between Worlds", "target": "Cadence of Hyrule", "value": 1},
-//         {"source": "Cadence of Hyrule", "target": "Triforce Heroes", "value": 1},
-//         {"source": "Triforce Heroes", "target": "Oracle of Seasons", "value": 1},
-//         {"source": "Oracle of Seasons", "target": "Link's Awakening", "value": 1},
-//         {"source": "Link's Awakening", "target": "Zelda I", "value": 1},
-//         {"source": "Zelda I", "target": "Zelda II: The Adventure of Link", "value": 1},
-//         {"source": "Zelda II: The Adventure of Link", "target": "CDi Games & Cartoon", "value": 1},
-//         {"source": "CDi Games & Cartoon", "target": "Breath of the Wild", "value": 1},
-//         {"source": "CDi Games & Cartoon", "target": "Hyrule Warriors: Age of Calamity", "value": 1},
-//         {"source": "Breath of the Wild", "target": "Tears of the Kingdom", "value": 1}
-//     ]
-// }
