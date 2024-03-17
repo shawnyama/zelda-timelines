b928704d9b8f02fadb36014c89b4f330a4c84f73
Shawn Yama <syama@uncharted.software>
2024-03-17 17:02:38 -0400

    prepare build

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+declare module 'vue-mermaid-string' {
+  import { DefineComponent } from 'vue'
+
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
