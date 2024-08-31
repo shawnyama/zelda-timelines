bae665e4e1d72c85bee6d947fdec0479891d2d86
Shawn Yama <syama@uncharted.software>
2024-08-30 22:01:33 -0400

    copy readme for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), ViteImageOptimizer()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'README.md',
+          dest: ''
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
