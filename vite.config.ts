325285b2f8cfc450469bcb49f872be34792b206f
Shawn Yama <syama@uncharted.software>
2024-08-31 21:09:53 -0400

    avoid the copying in dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    vue(),
-    ViteImageOptimizer(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'README.md',
-          dest: ''
-        }
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  const plugins: Plugin[] = [vue(), ViteImageOptimizer()]
+
+  if (command === 'build') {
+    plugins.push(
+      ...viteStaticCopy({
+        targets: [
+          {
+            src: 'README.md',
+            dest: ''
+          }
+        ]
+      })
+    )
+  }
+
+  return {
+    base: '/',
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      target: 'esnext'
     }
-  },
-  build: {
-    target: 'esnext'
   }
 })
