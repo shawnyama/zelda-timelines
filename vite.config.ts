eff6890054209da3fe31255e1df127809c0ea36b
Shawn Yama <shawnyama@Shawns-MacBook-Pro.local>
2025-05-25 12:27:26 -0400

    enable dot files when deploying

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
 
   if (command === 'build') {
     plugins.push(
-      ...viteStaticCopy({
+      viteStaticCopy({
         targets: [
           {
             src: 'README.md',
